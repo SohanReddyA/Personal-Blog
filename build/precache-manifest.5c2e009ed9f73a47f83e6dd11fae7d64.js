a4188ae5048907bdff4affa711d7dc9070362fe3
Sohan Reddy <pingusohan@gmail.com>
2020-06-24 22:42:31 +0530

    fixed errors

---
+++ build/precache-manifest.5c2e009ed9f73a47f83e6dd11fae7d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278a30cc6af7ed6553996dfc4b3e4320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ff790bf420438f6184a",
+    "url": "/static/css/main.bf704337.chunk.css"
+  },
+  {
+    "revision": "5fc3f2869cb1bcfd4941",
+    "url": "/static/js/2.f54605c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f54605c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff790bf420438f6184a",
+    "url": "/static/js/main.4564e4a5.chunk.js"
+  },
+  {
+    "revision": "e9b3aec5b931f5177448",
+    "url": "/static/js/runtime-main.98d32e4b.js"
+  }
+]);
