4c9f5027400d39e3938efe4169279bd533b608d0
Sohan Reddy <pingusohan@gmail.com>
2020-06-24 22:32:47 +0530

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const express = require('express');
+const app = express();
+const http = require('http');
+
+const port = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+server.listen(port);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
